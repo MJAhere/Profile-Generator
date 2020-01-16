9d619ac4877cf49301146f7d08033566c81409cb
Michael Aiello <mjahere023@gmail.com>
2020-01-16 09:51:28 -0700

    Created 3 js files and added css and html.

---
+++ index.js
@@ -0,0 +1,26 @@
+const questions = [ {
+    type: "input",
+    name: "gitHub",
+    message: "What is your github user name."
+
+},
+{
+    type: "list",
+    name: "color",
+    message: "What is your favorite color.",
+    choices: ["green", "blue", "pink", "red"]
+
+
+
+}
+
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init();
